764ff9c326f11f927c26e273dbc8e8e30bf3e642
emma074 <138093577+emma074@users.noreply.github.com>
2025-07-14 23:20:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+
+class Solution {
+    public int nthFibonacci(int n) {
+        // code here
+        if(n==1){
+            return 1;
+        }
+        if(n==0) return 0;
+        return nthFibonacci(n-1)+nthFibonacci(n-2);
+    }
+}
